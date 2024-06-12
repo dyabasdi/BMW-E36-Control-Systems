87ad3e9306e28536222582fa80fefe6518e539bf
Dyab Asdi <133081624+dyabasdi@users.noreply.github.com>
2024-06-11 22:13:27 -0500

    Update VehControls-wheelSpeed.h

--- Vehicle Controls - Arduino/VehControls-wheelSpeed.h
+++ Vehicle Controls - Arduino/VehControls-wheelSpeed.h
@@ -2,6 +2,12 @@
 #define WHEELSPEED_H
 #include <Arduino.h>
 
+extern const int sensorPins[];
+extern volatile unsigned long wheelPulses[];
+extern const float wheelCircumference;
+extern const unsigned long timeInterval;
+extern unsigned long previousTime;
+
 void wheelSpeedSetup();
 void wheelSpeedCalc(unsigned long wheelSpeeds[4]);
 
