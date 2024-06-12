ce2ec3fc29f6cc9b057a1cc38a8bac00ec68ca42
Dyab Asdi <133081624+dyabasdi@users.noreply.github.com>
2024-06-11 22:12:14 -0500

    Update VehControls-Traction_Control.h

--- Vehicle Controls - Arduino/VehControls-Traction_Control.h
+++ Vehicle Controls - Arduino/VehControls-Traction_Control.h
@@ -2,6 +2,17 @@
 #define TRACTIONCONTROL_H
 #include <Arduino.h>
 
+extern float Kp;
+extern float Ki;
+extern float Kd;
+extern const float desiredSlip;
+extern double slip;
+extern double prevError;
+extern double integral;
+extern double throttleOutput;
+extern unsigned long prevTime;
+extern const unsigned long sampleRate;
+
 void tractionControlSetup();
 void tractionControlAlgorithm(unsigned long wheelSpeeds[4]);
 
